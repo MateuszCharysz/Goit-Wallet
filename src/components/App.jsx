6b84fcd71117c88ce81bf36157324f831f4eba8b
MateuszCharysz <charyszek@interia.pl>
2023-09-19 00:54:01 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import React, { useState, lazy } from 'react';
 // import reactLogo from '../assets/react.svg';
 // import viteLogo from '/vite.svg';
-import './App.css';
+import './app.css';
 
 // const Home = lazy(() => import('./pages/home/Home'));
 
