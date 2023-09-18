e023ca698b3e963a2bbdf9af61ee730461e919d5
MateuszCharysz <charyszek@interia.pl>
2023-09-19 00:55:26 +0200

    quick fix 2

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app';
+import App from './components/app.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import 'modern-normalize';
