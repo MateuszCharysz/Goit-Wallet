27a4ab03dff8f346d608f1d3645f119e031a91b6
MaggieWesolowska <maggiewes07@gmail.com>
2023-09-20 16:47:31 +0200

    edited styles for login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,14 +2,17 @@
 import React from 'react';
 import LoginForm from '../components/LoginForm/LoginForm';
 import logo from '../images/logo.svg';
+import css from '../components/LoginForm/LoginForm.module.css';
 
 const Login = () => {
   return (
     <>
       <div>
-        <img src={logo} alt='wallet-logo'></img>
+        <img
+          className={css.logo}
+          src={logo}
+          alt='wallet-logo'></img>
       </div>
-
       <LoginForm />
     </>
   );
