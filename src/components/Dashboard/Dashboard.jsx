5d72caa9819f916aaca2f38a30170324250a915e
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-09-22 11:30:28 +0200

    connect dashboard to DB

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,95 +1,110 @@
 import styles from "./Dashboard.module.css"
-import editImage from "../../assets/edit.svg"
-import balance from "../../../src/balance.json"
+// import balance from "../../../src/balance.json"
 import { DeleteButton } from "../DeleteButton/DeleteButton"
 import { EditPen } from "../EditPen/EditPen"
+import { useEffect, useState } from "react"
 
-const handleEdit = ({id}) => {
-    console.log(`Tu pojawi się modal do edycji danych. ${id}`)
-}
 export const Dashboard = () => {
 
-    return (
+ const [data, setData] = useState();
 
-      <table className={styles.dashboardClass}>
-    <thead>
-      <tr>
-        <th>Date</th>
-        <th>Type</th>
-        <th>Category</th>
-        <th>Comment</th>
-        <th>Sum</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody className={styles.dashboardClassBody}>
-      {balance.map(({ id, date, type, category, comment, sum })=>{
-        const time = new Date(parseInt(date))
-        const getDay = (time) => {
-        const day = new Date(time).getDate()
-            if (day<10) {
-                return `0${day}`
-            }   return day
-        }
-        const getMonth = (time) => {
-            const month = new Date(time).getMonth()+1
-                if (month<10) {
-                    return `0${month}`
-                } return month
-            }
-        const getYear = (time) => {
-            const trim = (number) => {
-                if (number<10) {
-                    return `0${number}`
-                }   return number
-            }
-            const year = new Date(time).getYear()
-                if (year<100) {
-                    return trim(year)
-                } return trim(year-100)
-        }
-        if (type == '+') {
-            return(
-            <tr key={id}>
-                <td>
-                    {getDay(time)}.
-                    {getMonth(time)}.
-                    {getYear(time)}
-                </td>
-                <td>{type}</td>
-                <td>{category}</td>
-                <td>{comment}</td>
-                <td className={styles.green}>{sum}</td>
-                <td>
-                <span className={styles.buttonContainer}>
-                    <EditPen id={id} type={type}/>
-                    <DeleteButton name="DELETE" id={id}/>
-                    </span>
-                </td>
-            </tr>
-            )
-        }
-            return(
-            <tr key={id}>
-                <td>
-                    {getDay(time)}.
-                    {getMonth(time)}.
-                    {getYear(time)}
-                </td>
-                <td>{type}</td>
-                <td>{category}</td>
-                <td>{comment}</td>
-                <td className={styles.red}>{sum}</td>
-                <td>
-                <span className={styles.buttonContainer}>
-                    <EditPen id={id} type={type}/>
-                    <DeleteButton name="DELETE" id={id}/>
-                    </span>
-                </td>
-            </tr>
-            )
-      })}
-  
-    </tbody>
-      </table>
+ useEffect(() => {
+  const dataFetch = async () => {
+    const results = await (
+      await fetch(
+        "https://cosmic-answer-399520.lm.r.appspot.com/wallet/api/mockTransactions"
+      )
+    ).json();
+    const data = results.data
+    setData(data);
+  };
+  dataFetch();
+}, []);
+
+return (
+    <>{data ? 
+        <table className={styles.dashboardClass}>
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Type</th>
+                    <th>Category</th>
+                    <th>Comment</th>
+                    <th>Sum</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody className={styles.dashboardClassBody}>
+            {data.map(({ _id, date, type, category, comment, sum })=>{
+                const time = new Date(parseInt(date))
+                const getDay = (time) => {
+                const day = new Date(time).getDate()
+                    if (day<10) {
+                        return `0${day}`
+                    }   return day
+                }
+                const getMonth = (time) => {
+                    const month = new Date(time).getMonth()+1
+                        if (month<10) {
+                            return `0${month}`
+                        } return month
+                    }
+                const getYear = (time) => {
+                    const trim = (number) => {
+                        if (number<10) {
+                            return `0${number}`
+                        }   return number
+                    }
+                    const year = new Date(time).getYear()
+                        if (year<100) {
+                            return trim(year)
+                        } return trim(year-100)
+                }
+                if (type == '+') {
+                    return(
+                    <tr key={_id}>
+                        <td>
+                            {getDay(time)}.
+                            {getMonth(time)}.
+                            {getYear(time)}
+                        </td>
+                        <td>{type}</td>
+                        <td>{category}</td>
+                        <td>{comment}</td>
+                        <td className={styles.green}>{sum}</td>
+                        <td>
+                        <span className={styles.buttonContainer}>
+                            <EditPen id={_id} type={type}/>
+                            <DeleteButton name="DELETE" id={_id}/>
+                            </span>
+                        </td>
+                    </tr>
+                    )
+                }
+                    return(
+                    <tr key={_id}>
+                        <td>
+                            {getDay(time)}.
+                            {getMonth(time)}.
+                            {getYear(time)}
+                        </td>
+                        <td>{type}</td>
+                        <td>{category}</td>
+                        <td>{comment}</td>
+                        <td className={styles.red}>{sum}</td>
+                        <td>
+                        <span className={styles.buttonContainer}>
+                            <EditPen id={_id} type={type}/>
+                            <DeleteButton name="DELETE" id={_id}/>
+                            </span>
+                        </td>
+                    </tr>
+                    )
+            })}
+            </tbody>
+        </table>:
+        <div>
+            <h1 className={styles.loader}>Loading...</h1>
+        </div>}
+      </>
   )};
