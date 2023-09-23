c143a3972556d2cd045f6470b5c1ab7aa1176980
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-09-23 15:10:17 +0200

    modal

---
+++ src/components/SelectMenuModal/SelectMenuModal.jsx
@@ -0,0 +1,56 @@
+import React, {useEffect, useState} from "react";
+import styles from "../SelectMenuModal/SelectMenuModal.module.css"
+
+
+
+const categories = [
+    {'category': 'Main expenses'},
+    {'category': 'Products'},
+    {'category': 'Car'},
+    {'category': 'Self care'},
+    {'category': 'Child care'},
+    {'category': 'Household products'},
+    {'category': 'Education'},
+    {'category': 'Leisure'},
+    {'category': 'Other expenses'}]
+
+const data = categories
+console.log(data)
+
+export const SelectMenuModal = ({ placeholder }) => {
+
+    const [modal, setModal] = useState(false)
+    const [name, setName] = useState(`${placeholder}`)
+
+    const toogleModal = () => {
+        setModal(!modal)
+    }
+    const changeName = (e) => {
+        const newName = e.innerText
+        setName(newName)
+        setModal(!modal)
+        return newName
+    }
+
+    useEffect(()=>{
+    console.log('dokonano zmiany')
+    }),[name]
+
+    return (
+        <>
+        <div className={styles.wrapper}>
+            <div onClick={toogleModal} className={styles.selectBtn}>
+                <span>{name}</span>
+            </div>
+        {modal&&
+            <div className={styles.optionsContainer}>
+                <ul className={styles.options}>
+                    {data.map(({category})=>{return(<li onClick={(e)=>changeName(e.target)} className={styles.option}><span>{category}</span></li>)})}
+                </ul>
+            </div>
+        }
+        </div>
+        </>
+    )
+}
+
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Dashboard } from '../components/Dashboard/Dashboard'
 import { AddTransactionButton } from '../components/AddTransactionButton/AddTransactionButton'
+import { SelectMenuModal } from '../components/SelectMenuModal/SelectMenuModal'
 
 export const DashBoard = () => {
   return (
     <>
     <Dashboard/>
     <AddTransactionButton/>
+    <SelectMenuModal placeholder={'Select a category'}/>
     </>
   )
 }
