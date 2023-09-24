0ce2046b728f98e68dbd4e1beebc658fffee36e5
Beniamin Brzeziński <benek9311@wp.pl>
2023-09-24 18:50:48 +0200

    Added redux with persist token, hook useAuth, PrivateRoute and RestrictedRoute

---
+++ src/redux/auth/actions.js
@@ -0,0 +1,86 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://wallet-api.cyclic.cloud/api';
+
+const setAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+const clearAuthHeader = () => {
+  axios.defaults.headers.common.Authorization = '';
+};
+
+/*
+ * POST @ /users/register
+ * body: { username, email, password }
+ */
+export const register = createAsyncThunk(
+  'auth/register',
+  async (credentials, thunkAPI) => {
+    try {
+      const res = await axios.post('/users/register', credentials);
+      return res.data.result;
+    } catch (err) {
+      console.error(err.message);
+      return thunkAPI.rejectWithValue('Registration failed');
+    }
+  }
+);
+
+/*
+ * POST @ /users/login
+ * body: { email, password }
+ */
+export const login = createAsyncThunk(
+  'auth/login',
+  async (credentials, thunkAPI) => {
+    try {
+      const res = await axios.post('/users/login', credentials);
+      setAuthHeader(res.data.result.token);
+      return res.data.result;
+    } catch (err) {
+      console.error(err.message);
+      return thunkAPI.rejectWithValue('Login failed');
+    }
+  }
+);
+
+/*
+ * POST @ /users/logout
+ * headers: Authorization: Bearer token
+ */
+export const logout = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
+  try {
+    await axios.post('/users/logout');
+    clearAuthHeader();
+  } catch (err) {
+    console.error(err.message);
+    return thunkAPI.rejectWithValue('Logout failed');
+  }
+});
+
+/*
+ * GET @ /users/current
+ * headers: Authorization: Bearer token
+ */
+export const refreshUser = createAsyncThunk(
+  'auth/refreshUser',
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+
+    if (!persistedToken) {
+      return thunkAPI.rejectWithValue('Unable to fetch user');
+    }
+
+    try {
+      setAuthHeader(persistedToken);
+      const res = await axios.get('/users/current');
+      return res.data.result;
+    } catch (err) {
+      console.error(err.message);
+      return thunkAPI.rejectWithValue('User refresh failed');
+    }
+  }
+);
