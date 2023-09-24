4be9b50da8d2945ed1fcfe82684d55ae14d8b597
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-09-24 16:55:10 +0200

    Update SelectMenuModal.jsx

--- src/components/SelectMenuModal/SelectMenuModal.jsx
+++ src/components/SelectMenuModal/SelectMenuModal.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import styles from "../SelectMenuModal/SelectMenuModal.module.css"
 import { nanoid } from "nanoid";
 
