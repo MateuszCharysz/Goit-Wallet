06968df9d9e45e8e7ce9ddf3088d2d6d2adbea97
MaggieWesolowska <maggiewes07@gmail.com>
2023-09-25 21:35:22 +0200

    edited styles for register

--- src/pages/registration.jsx
+++ src/pages/registration.jsx
@@ -3,10 +3,15 @@ import css from '../components/RegistrationForm/RegistrationForm.module.css';
 import logo from '../utils/Svg/logo.svg';
 import RegistrationForm from '../components/RegistrationForm/RegistrationForm';
 import frameTablet from '../utils/frame-registration/frame-tablet.png';
+import frameDesk from '../utils/frame-registration/frame-desktop.png';
 
 const Registration = () => {
   return (
     <div className={css.registerContainer}>
+      <div className={css.desktopContainer}>
+        <img src={frameDesk}></img>
+        <span className={css.finance}>Finance App</span>
+      </div>
       <div className={css.tabletContainer}>
         <img src={frameTablet}></img>
         <span className={css.finance}>Finance App</span>
