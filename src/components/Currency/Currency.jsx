4c138655a329c8f8953e7cf4a8b2852561eafddb
MarcinZbor <marcinzb@gazeta.pl>
2023-09-25 15:06:40 +0200

    Poprawione bledy
    dodalem svg z fala, poprawilem bledy

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -1,61 +1,62 @@
 import React, { useState } from "react";
 import styles from "./Currency.module.css";
-import { getCurrency } from "./getCurrency";
-import Loader from "components/Loader/Loader";
-import { useSelector } from "react-redux";
-import { selectToken } from "redux/auth/auth-selectors";
+// import { getCurrency } from "./getCurrency";
+// import Loader from "components/Loader/Loader";
+// import { useSelector } from "react-redux";
+// import { selectToken } from "redux/auth/auth-selectors";
 
-function Currency() {
-	const [currencyData, setCurrencyData] = useState(null);
-	const [isLoading, setIsLoading] = useState(false);
-	const token = useSelector(selectToken);
+const Currency = () => {
+	// const [currencyData, setCurrencyData] = useState(null);
+	// const [isLoading, setIsLoading] = useState(false);
+	// const token = useSelector(selectToken);
 
-	async function fetchData() {
-		const data = await getCurrency(token);
-		setCurrencyData(data);
-		setIsLoading(true);
-	}
+	// async function fetchData() {
+	// 	const data = await getCurrency(token);
+	// 	setCurrencyData(data);
+	// 	setIsLoading(true);
+	// }
 
-	if (!isLoading) {
-		fetchData();
-		setInterval(fetchData, 6000000);
-		return (
-			<div className={styles.currency}>
-				<Loader />
-			</div>
-		);
-	}
+	// if (!isLoading) {
+	// 	fetchData();
+	// 	setInterval(fetchData, 6000000);
+	// 	return (
+	// 		<div className={styles.currency}>
+	// 			<Loader />
+	// 		</div>
+	// 	);
+	// }
 
-	const eurBuy = currencyData.eur.buy;
-	const eurSale = currencyData.eur.sale;
-	const USDSale = currencyData.usd.sale;
-	const USDBuy = currencyData.usd.buy;
+	// const eurBuy = currencyData.eur.buy;
+	// const eurSale = currencyData.eur.sale;
+	// const USDSale = currencyData.usd.sale;
+	// const USDBuy = currencyData.usd.buy;
 
 	return (
 		<div className={styles.currency}>
-			<table className={styles.currency__tbl}>
-				<thead className={styles.currency__thead}>
+			<table className={styles.currencyTbl}>
+				<thead className={styles.currencyThead}>
 					<tr>
-						<th className={styles.currency__tbl_title}>Currency</th>
-						<th className={styles.currency__tbl_title}>Purchase</th>
-						<th className={styles.currency__tbl_title}>Sale</th>
+						<th className={styles.currencyTblTitle}>Currency</th>
+						<th className={styles.currencyTblTitle}>Purchase</th>
+						<th className={styles.currencyTblTitle}>Sale</th>
 					</tr>
 				</thead>
-				<tbody className={styles.currency__tbody}>
+				<tbody className={styles.currencyTbody}>
 					<tr>
-						<td className={styles.currency__tbl_item}>USD</td>
-						<td className={styles.currency__tbl_item}>{USDBuy}</td>
-						<td className={styles.currency__tbl_item}>{USDSale}</td>
+						<td className={styles.currencyTblItem}>USD</td>
+						<td className={styles.currencyTblItem}>50</td>
+						<td className={styles.currencyTblItem}>80</td>
 					</tr>
 					<tr>
-						<td className={styles.currency__tbl_item}>EUR</td>
-						<td className={styles.currency__tbl_item}>{eurBuy}</td>
-						<td className={styles.currency__tbl_item}>{eurSale}</td>
+						<td className={styles.currencyTblItem}>EUR</td>
+						<td className={styles.currencyTblItem}>100</td>
+						<td className={styles.currencyTblItem}>200</td>
 					</tr>
 				</tbody>
 			</table>
+			
 		</div>
 	);
-}
+};
 
 export default Currency;
---
+++ src/components/Currency/getCurrency.js
@@ -0,0 +1,37 @@
+// import axios from 'axios';
+// function getCurrency(token) {
+//   const response = await axios(
+//     // WPISAC ADRES
+//     {
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     }
+//   );
+
+//   const currencyData = {};
+
+//   response.data.currency.forEach(currency => {
+//     switch (currency.ccy) {
+//       case 'EUR':
+//         currencyData.eur = {
+//           buy: currency.buy,
+//           sale: currency.sale,
+//         };
+//         break;
+//       case 'USD':
+//         currencyData.usd = {
+//           buy: currency.buy,
+//           sale: currency.sale,
+//         };
+//         break;
+
+//       default:
+//         break;
+//     }
+//   });
+
+//   return currencyData;
+// }
+
+// export default getCurrency
