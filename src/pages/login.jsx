20cc801ff1096a4fd8ebdbdc1b8e6a6c29620ab6
MaggieWesolowska <maggiewes07@gmail.com>
2023-09-26 15:46:39 +0200

    relocated folders, added img

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -4,10 +4,16 @@ import logo from '../utils/Svg/logo.svg';
 import css from '../components/LoginForm/LoginForm.module.css';
 import frameTablet from '../utils/frame-login/frame-tablet.png';
 import frameDesk from '../utils/frame-login/frame-desktop.png';
+import ellipseTablet from '../utils/bg-tablet-login/ellipse-up.png';
 
 const Login = () => {
   return (
     <div className={css.loginContainer}>
+      <div className={css.ellipseTablet}>
+        <img
+          className={css.ellipseUp}
+          src={ellipseTablet}></img>
+      </div>
       <div className={css.desktopContainer}>
         <img src={frameDesk}></img>
         <span className={css.finance}>Finance App</span>
